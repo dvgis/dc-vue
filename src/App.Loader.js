48099558237a954b57a7adc92e7173a3e1da3551
Caven Chen <cavencj@gmail.com>
2020-07-29 17:13:56 +0800

    修改依赖

--- src/App.Loader.js
+++ src/App.Loader.js
@@ -2,12 +2,13 @@
  * @Author: Caven
  * @Date: 2020-03-19 22:36:19
  * @Last Modified by: Caven
- * @Last Modified time: 2020-07-15 18:10:10
+ * @Last Modified time: 2020-07-29 17:07:35
  */
 import Vue from 'vue'
 import 'dvgis/dc-sdk/dist/dc.base.min'
 import 'dvgis/dc-sdk/dist/dc.core.min'
 import 'dvgis/dc-sdk/dist/dc.core.min.css'
+import 'dvgis/dc-plugins/dist/dc.plugins.min'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
